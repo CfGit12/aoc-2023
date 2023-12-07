fa19387daa5fbf23ab654fba47868fc19c9e0703
Charles Flynn <charles.flynn@fanduel.com>
2023-12-07 18:30:37 +0000

    day 7 shorter

--- src/main/kotlin/7.kt
+++ src/main/kotlin/7.kt
@@ -24,69 +24,22 @@ class HandAndBid(
     private val jacksWild: Boolean
 ) : Comparable<HandAndBid> {
 
-    override fun toString() = "$cards - $bid - $type"
+    override fun toString() = "$cards - $bid - $cardCounts"
 
-    private val type = getType()
-
-    private fun getType(): HandType =
-        if (jacksWild && cards.contains('J')) {
-            val withoutJacks = cards.filter { it != 'J' }
-            val jacksCount = cards.length - withoutJacks.length
-            when (jacksCount) {
-                5 -> HandType.FIVE_OF_A_KIND
-                4 -> HandType.FIVE_OF_A_KIND
-                3 -> if (withoutJacks.isPair()) HandType.FIVE_OF_A_KIND else HandType.FOUR_OF_A_KIND
-                2 -> {
-                    when {
-                        withoutJacks.isThreeOfAKind() -> HandType.FIVE_OF_A_KIND
-                        withoutJacks.isPair() -> HandType.FOUR_OF_A_KIND
-                        else -> HandType.THREE_OF_A_KIND
-                    }
+    private val cardCounts =
+        if (jacksWild) {
+            listOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
+                .map { originalHand ->
+                    val newHand = cards.replace('J', originalHand)
+                    newHand.cardCounts()
                 }
-
-                1 -> {
-                    when {
-                        withoutJacks.isFourOfAKind() -> HandType.FIVE_OF_A_KIND
-                        withoutJacks.isThreeOfAKind() -> HandType.FOUR_OF_A_KIND
-                        withoutJacks.isTwoPair() -> HandType.FULL_HOUSE
-                        withoutJacks.isPair() -> HandType.THREE_OF_A_KIND
-                        else -> HandType.PAIR
-                    }
-                }
-
-                else -> error("not possible")
-            }
+                .sortedWith(compareBy({ it[0] }, { it.getOrNull(1) }))
+                .last()
         } else {
-            when {
-                cards.isFiveOfAKind() -> HandType.FIVE_OF_A_KIND
-                cards.isFourOfAKind() -> HandType.FOUR_OF_A_KIND
-                cards.isFullHouse() -> HandType.FULL_HOUSE
-                cards.isThreeOfAKind() -> HandType.THREE_OF_A_KIND
-                cards.isTwoPair() -> HandType.TWO_PAIR
-                cards.isPair() -> HandType.PAIR
-                else -> HandType.HIGH_CARD
-            }
+            cards.cardCounts()
         }
 
-    private fun String.isFiveOfAKind() =
-        any { cardCount(it) == 5 }
-
-    private fun String.isFourOfAKind() =
-        any { cardCount(it) == 4 }
-
-    private fun String.isFullHouse() =
-        any { cardCount(it) == 3 } && any { cardCount(it) == 2 }
-
-    private fun String.isThreeOfAKind() =
-        any { cardCount(it) == 3 }
-
-    private fun String.isTwoPair() =
-        count { cardCount(it) == 2 } == 4
-
-    private fun String.isPair() =
-        any { cardCount(it) == 2 }
-
-    private fun String.cardCount(card: Char) = count { it == card }
+    private fun String.cardCounts() = groupingBy { it }.eachCount().values.sortedDescending()
 
     private fun Char.toRank() = when {
         this == 'T' -> 10
@@ -101,16 +54,12 @@ class HandAndBid(
     override fun compareTo(other: HandAndBid) = compareValuesBy(
         this,
         other,
-        { it.type },
+        { it.cardCounts[0] },
+        { it.cardCounts.getOrNull(1) },
         { it.cards[0].toRank() },
         { it.cards[1].toRank() },
         { it.cards[2].toRank() },
         { it.cards[3].toRank() },
         { it.cards[4].toRank() }
     )
-
-}
-
-private enum class HandType {
-    HIGH_CARD, PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND
 }
